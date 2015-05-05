f56da70365efcfdc6cd90a95217e8803eea3eb1d
Kyle Waid <kyle.waid@gcotech.com>
2015-05-04 23:36:43 -0400

    First Commit

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Show Product Tree First',
+    'version': '1.1',
+    'author': 'Kyle Waid',
+    'category': 'Sales Management',
+    'depends': ['product'],
+    'website': 'https://www.gcotech.com',
+    'description': """
+    """,
+    'data': ['views/product.xml',
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
